579ac00050d138e8d8472c22c2ddc5c360b685ca
Drew Page <drew@siegemedia.com>
2021-03-13 18:01:52 -0800

    implementing next dynamic routing

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,18 @@ import Head from 'next/head'
 // import Nav from '../components/nav'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../components/styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -13,6 +23,20 @@ export default function Home() {
       <section className={utilStyles.headingMD}>
         <p>Hello my name is Drew</p>
         <p>What else do you really need to know?</p>
+      </section>
+      <section className={`${utilStyles.headingMD} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLG}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem}>
+              {title}
+              <br />
+              {date}
+              <br />
+              {id}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
